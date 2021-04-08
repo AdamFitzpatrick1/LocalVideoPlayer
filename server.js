6dd454faf1055c21cc5d3d02892132d8b976a8ed
Adam Fitzpatrick <1257498@stu.tmc.ac.uk>
2021-04-08 17:22:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,46 +2,52 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/video", function (req, res) {
-  // Ensure there is a range given for the video
-  const range = req.headers.range;
-  if (!range) {
-    res.status(400).send("Requires Range header");
-  }
+app.get("/video", function(req, res) {
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
 
-  // get video stats (about 61MB)
-  const videoPath = "example.mp4";
-  const videoSize = fs.statSync("example.mp4").size;
+    // get video stats (about 61MB)
+    const videoPath = "example.mp4";
+    const videoSize = fs.statSync("example.mp4").size;
 
-  // Parse Range
-  // Example: "bytes=32324-"
-  const CHUNK_SIZE = 10 ** 6; // 1MB
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-  // Create headers
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
 
-  // HTTP Status 206 for Partial Content
-  res.writeHead(206, headers);
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
 
-  // create video read stream for this particular chunk
-  const videoStream = fs.createReadStream(videoPath, { start, end });
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
 
-  // Stream the video chunk to the client
-  videoStream.pipe(res);
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
 });
 
-app.listen(8000, function () {
-  console.log("Listening on port 8000!");
+app.listen(8000, function() {
+    console.log("\n");
+    console.log("-----------------------------------------");
+    console.log("Welcome to the Node.js Local Video Player");
+    console.log("-----------------------------------------");
+    console.log("\n");
+    console.log("Listening on port 8000!");
+    console.log("Navigate to localhost:8000 in your browser to view the page");
 });
